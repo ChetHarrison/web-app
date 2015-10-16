6d45af7fc249570228a90a45f8de4dc4f33841e3
Chet Harrison <chet.harrison@gmail.com>
2015-10-16 09:21:42 -0700

    Removed files from old lib dir

--- dev/es5/lib/print.js
+++
@@ -1,8 +0,0 @@
-define(["exports", "module"], function (exports, module) {
-  "use strict";
-
-  module.exports = function (msg) {
-    return console.warn(msg);
-  };
-});
-//# sourceMappingURL=print.js.map
--- dev/es6/lib/print.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-export default ( msg ) => console.warn( msg );
